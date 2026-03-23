4360e5449b9fb2e95de82710f16eccea76036d5a
itskenzie <m.ilhammf1432@gmail.com>
2026-03-24 05:49:54 +0700

    Update subdomain-availability.js

--- scripts/subdomain-availability.js
+++ scripts/subdomain-availability.js
@@ -15,10 +15,10 @@ const userGitHubName = document.getElementById("user-gh-name");
 async function fetchReservedDomains() {
     try {
         if (!reservedDomains) {
-            const res1 = await fetch("https://raw.githubusercontent.com/is-a-dev/register/main/util/reserved.json");
+            const res1 = await fetch("https://raw.githubusercontent.com/zyrocfnd/freedomain/refs/heads/main/util/reserved.json");
             if (!res1.ok) throw new Error(`Failed to fetch reserved domains: ${res1.statusText}`);
 
-            const res2 = await fetch("https://raw.githubusercontent.com/is-a-dev/register/main/util/internal.json");
+            const res2 = await fetch("https://raw.githubusercontent.com/zyrocfnd/freedomain/refs/heads/main/util/internal.json");
             if (!res2.ok) throw new Error(`Failed to fetch internal domains: ${res2.statusText}`);
 
             reservedDomains = [...await res1.json(), ...await res2.json()];
@@ -63,7 +63,7 @@ async function check() {
 
     isSearching = true;
     try {
-        const response = await fetch(`https://raw.githubusercontent.com/is-a-dev/register/main/domains/${search}.json`);
+        const response = await fetch(`https://raw.githubusercontent.com/zyrocfnd/freedomain/main/domains/${search}.json`);
         if (!response.ok && response.status !== 404) throw new Error("Something went wrong");
 
         const data = response.ok ? await response.json() : null;
